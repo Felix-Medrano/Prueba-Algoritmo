7aea2a3268f0d150acd321b6708a3b7cdb4544b7
Felix Medrano <sckrool@gmail.com>
2022-02-03 22:03:32 -0600

    Agregar archivos de proyecto.

---
+++ Prueba Algoritmo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prueba_Algoritmo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
